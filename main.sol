b5243337cb3bfc048084aaf99b90356f559d5158
Fabio Santos Alves <fa3io.santos.alves@gmail.com>
2025-08-10 22:19:42 -0300

    Ajuste em contrato

--- main.sol
+++ main.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    uint256 private storedData;
+    //Inteiro com sinal
+    int private numeroComSinal;
+    //Inteiro sem sinal
+    uint256 private numeroSemSinal;
+    //Booleano verdadeiro ou falso
+    bool private ;
+
 
     function set(uint256 x) public {
-        storedData = x;
+        numeroSemSinal = x;
     }
 
     function get() public view returns (uint256) {
-        return storedData;
+        return numeroSemSinal;
     }
 }
